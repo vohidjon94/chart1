ca0c26bcbe27272c59e730cb1eb9155be23031bf
vohidjon94 <vitcoderuz@gmail.com>
2022-04-04 17:05:46 +0500

    yangilandi_4_04

--- core/views.py
+++ core/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'index.html', context)
+
+def example(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'examp.html', context)
