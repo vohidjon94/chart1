a3f87d4492a92966536cfc99ccb0f2088fba3370
vohidjon94 <vitcoderuz@gmail.com>
2022-04-06 13:37:50 +0500

    addcharts

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,13 @@ from .views import *
 
 urlpatterns = [
     path('', home),
-    path("example/", example)
+    path("example/", example),
+    path("area/", area),
+    path("bar/", bar),
+    path("bubble/", bubble),
+    path("doughnut/", doughnut),
+    path("line/", line),
+    path("polar/", polar),
+    path("radar/", radar),
+    path("scatter/", scatter)
 ]
--- core/views.py
+++ core/views.py
@@ -16,3 +16,51 @@ def example(request):
         'posts': posts
     }
     return render(request, 'examp.html', context)
+def area(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'area.html', context)
+def bar(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'bar.html', context)
+def bubble(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'bubble.html', context)
+def doughnut(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'doughnut.html', context)
+def line(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'line.html', context)
+def polar(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'polar.html', context)
+def radar(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'radar.html', context)
+def scatter(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'scatter.html', context)
